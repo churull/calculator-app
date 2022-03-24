13b6dc25bd2e5f927a51ab7be3492ece6d264ad9
churull <hasanahnurul976@gmail.com>
2022-03-24 12:31:40 +0700

    Tugas Object dan Class

---
+++ TugasObjectDanClass/TugasObjectDanClass/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TugasObjectDanClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //membuat objek taxi
+            Taxi taxi = new Taxi();
+
+            // pengisian nilai properties
+            taxi.DriverName = "Jihoon";
+            taxi.OnDuty = true;
+            taxi.NumPassenger = 80;
+
+            //pemanggilan method
+
+            taxi.TaxiInfo();
+            taxi.PickUpPassanger();
+            taxi.DropOffPassagner();
+
+            Console.ReadKey();
+        }
+    }
+}
